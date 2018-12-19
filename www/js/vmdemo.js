1d5c8ea45050d4e7a7ca9c038034cda89458498b
hoge1e3 <hoge1e3@gmail.com>
2018-12-19 10:20:22 +0900

    vm run ok

--- www/js/vmdemo.js
+++ www/js/vmdemo.js
@@ -1,15 +1,15 @@
 define(function (require,exports,module) {
     var parser=require("dolittle/minimal"),
         vm=require("dolittle/dtlvm"),
-        DU=require("DeferredUtil"),
         dummy=false;
-    exports.run=function (src) {
+    exports.compile=function (src) {
         var node=parser.parseAsNode(src);
         var vmc=vm.node2vm(node);
-        return DU.resolve({
-            code: vmc
-        });
+        return vmc;
     };
-    console.log(exports);
+    exports.run=function (vmc) {
+        return $.post("runDtl.php",{script:JSON.stringify(vmc)});
+    };
+    //console.log(exports);
     //return exports;
 });
--- www/runDtl.php
+++ www/runDtl.php
@@ -21,10 +21,10 @@ if (isset($_POST["script"]) || isset($_GET["file"])) {
         $scr=$_POST["script"];
     }
     $j=new Services_JSON;
-    $vmc=$j->decode($scr);
+    $vmc=json_decode/*$j->decode*/($scr);
 
     header("Content-type: text/json; charset=utf8");
-    echo $j->encode( DtlUtil::unwrap( Dtl::run($root,$vmc) ) );
+    echo json_encode/*$j->encode*/( DtlUtil::unwrap( Dtl::run($root,$vmc) ) );
 } else { ?>
     <form action="runDtl.php" method="POST">
     <textarea name="script" rows=10 cols=40>
